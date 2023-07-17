c888d121d1916bf9929f695ab895e11817a34d77
bmcafee <bennettjmcafee@gmail.com>
2023-07-16 19:48:42 -0500

    Moved Sparkling data to a seperate file, Sparkling.R
    Signed-off-by: bmcafee <bennettjmcafee@gmail.com>

--- DataCleaning_Scripts/Trout.R
+++ DataCleaning_Scripts/Trout.R
@@ -43,24 +43,3 @@ data_b <- data.frame("datetime" = data$sampledate,
 
 Trout <- rbind(Trout, data_b)
 rm(data_b)
-
-#Magnuson, J.J., S.R. Carpenter, and E.H. Stanley. 2023. North Temperate Lakes
-#LTER: High Frequency Water Temperature Data - Sparkling Lake Raft 1989
-#- current ver 24. Environmental Data Initiative. 
-#https://doi.org/10.6073/pasta/52ceba5984c4497d158093f32b23b76d 
-res <- read_data_entity_names(packageId = "knb-lter-ntl.116.28")
-raw <- read_data_entity(packageId = "knb-lter-ntl.116.28", entityId = res$entityId[3])
-data <- readr::read_csv(file = raw)
-
-data_a <- data.frame("datetime" = data$sampledate,
-                     "lake" = rep("Trout", nrow(data)),
-                     "depth" = data$depth,
-                     "variable" = rep("temp", nrow(data)),
-                     "unit" = rep("DEG_C", nrow(data)),
-                     "observation" = data$wtemp,
-                     "flag" = data$flag_wtemp) %>%
-  drop_na(observation)
-
-
-Trout <- rbind(Trout, data_a)
-rm(data_a)
